1d1473d021d39152413845dabfe86edb40f8e166
Dani Stem <57697224+Dani-Stem@users.noreply.github.com>
2021-06-10 18:03:31 -0700

    fixed spacing issues

--- handTrackMod.py
+++ handTrackMod.py
@@ -14,7 +14,7 @@ class handDetec():
         self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detecCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
-    def finsHands(self, img, draw=True):
+    def findHands(self, img, draw=True):
 
 
         imgRgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -29,22 +29,18 @@ class handDetec():
 
         return img
 
-    def findPosition(self, img, handNum=0, draw=True):
-
+    def findPosition(self, img, handNo=0, draw=True):
         lmList = []
         if self.results.multi_hand_landmarks:
-           myHand = self.results.multi_hand_landmarks[handNum]
-
-            # converts from the decimal prints the id and position
+            myHand = self.results.multi_hand_landmarks[handNo]
+            #converts from the decimal prints the id and position
             for id, lm in enumerate(myHand.landmark):
-            h, w, c = img.shape
-            cx, cy = int(lm.x * w), int(lm.y * h)
-            lmList.append([id, cx, cy])
-
-            if draw:
-                cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
-
-        return lmList
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+            return lmList
 
 
 
@@ -53,16 +49,17 @@ def main():
     pTime = 0
     cTime = 0
     # video input
-    cap = cv2.VideoCapture(0)
-    detec = handDetec
+    cap = cv2.VideoCapture(2)
+    detec = handDetec()
 
     while True:
         # grabs img converts to proper color values
         success, img = cap.read()
         img = detec.findHands(img)
         lmList = detec.findPosition(img)
-        if len(lmList) != 0:
-            print(lmList[4])
+
+        #if len(lmList) != 0:
+        #    print(lmList[4])
 
         # supports smooth fps pt1
         cTime = time.time()
